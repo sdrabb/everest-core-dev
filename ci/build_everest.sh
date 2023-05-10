4c2c9b5c76eed855a466bd7856a0054a56c22d99
LAD101work <96466764+LAD101work@users.noreply.github.com>
2023-05-10 11:34:00 +0200

    Add current branch
    Signed-off-by: LAD101work <96466764+LAD101work@users.noreply.github.com>

--- ci/build_everest.sh
+++ ci/build_everest.sh
@@ -29,6 +29,7 @@ sudo chown -R docker /cpm_cache
 sudo chown -R docker /results
 cd /checkout/everest-workspace/
 git clone https://github.com/EVerest/everest-core.git 
+git checkout $CURRENT_BRANCH
 mkdir -p /checkout/everest-workspace/everest-core/build
 cd /checkout/everest-workspace/everest-core/build
 

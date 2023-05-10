d79d17f5a594b5750a876f81a9145702b4df6141
LAD101work <96466764+LAD101work@users.noreply.github.com>
2023-05-10 13:54:43 +0200

    Test wait for mqtt server
    Signed-off-by: LAD101work <96466764+LAD101work@users.noreply.github.com>

--- ci/testsets/testset_01.sh
+++ ci/testsets/testset_01.sh
@@ -4,6 +4,8 @@ echo "##############   run everest-testing   #############"
 
 cd /checkout/everest-workspace/everest-core/tests
 
+sleep 5 # wait for mqtt server to be ready
+
 pytest-3 -s -vvv /checkout/everest-workspace/everest-core/tests/core_tests/startup_tests.py \
           --path /checkout/everest-workspace/everest-core --junitxml=results.xml -rf
 

8487ad7001c23f1e35a9bc3bf9af212df8819849
LAD101work <96466764+LAD101work@users.noreply.github.com>
2023-05-10 15:20:16 +0200

    Fix compose exit status
    Signed-off-by: LAD101work <96466764+LAD101work@users.noreply.github.com>

--- ci/testsets/testset_01.sh
+++ ci/testsets/testset_01.sh
@@ -9,6 +9,10 @@ sleep 5 # wait for mqtt server to be ready
 pytest-3 -s -vvv /checkout/everest-workspace/everest-core/tests/core_tests/startup_tests.py \
           --path /checkout/everest-workspace/everest-core --junitxml=results.xml -rf
 
+TESTSET_EXIT_STATUS=$?
+
 sudo cp results.xml /results/results_testset_01.xml
 
 echo "##############   done   #############"
+
+exit $TESTSET_EXIT_STATUS
